dbebd434029b56f185214cdff99478083df95b80
CarMa90 <el.matteotti@gmail.com>
2026-09-02 20:11:49 -0700

    feature errors handlers

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const { isCelebrateError } = require("celebrate");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+app.use((err, req, res, next) => {
+  console.log("ERROR COMPLETO:");
+  console.log(err);
+
+  if (isCelebrateError(err)) {
+    const params = err.details.get("params");
+
+    if (params) {
+      return res.status(400).send({
+        message: params.details[0].message,
+      });
+    }
+
+    const body = err.details.get("body");
+
+    if (body) {
+      return res.status(400).send({
+        message: body.details[0].message,
+      });
+    }
+  }
+
+  const { statusCode = 500, message } = err;
+
+  return res.status(statusCode).send({
+    message:
+      statusCode === 500 ? "An error has ocurred on the server" : message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
