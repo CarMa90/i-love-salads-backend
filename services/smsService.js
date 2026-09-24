d55c3ae4cfc7007395b5ff21371ef36ef48cd4e1
CarMa90 <el.matteotti@gmail.com>
2026-09-24 07:28:23 -0700

    update smsService.js

--- services/smsService.js
+++ services/smsService.js
@@ -16,24 +16,32 @@ module.exports.sendVerificationSms = async function sendVerificationSms(
     console.log(`[SIMULACIÓN SMS] Enviado a: ${fullNumber}`);
     console.log(`[CONTENIDO]: ${messageText}`);
     console.log("------------------------------------------------");
-    return Promise.resolve(true);
+    return true;
   }
 
-  // 🚀 SI ESTAMOS EN PRODUCCIÓN, se dispara el envío real usando fetch nativo
-  const response = await fetch("https://api.smsmasivos.com.mx/sms/send", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      apikey: SMS_API_KEY,
-    },
-    body: JSON.stringify({
-      numbers: phone,
-      message: messageText,
-      sender: "I Love Salads",
-      country_code: cleanCountryCode,
-    }),
-  });
+  try {
+    const res = await fetch("https://api.smsmasivos.com.mx/sms/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: SMS_API_KEY,
+      },
+      body: JSON.stringify({
+        numbers: phone,
+        message: messageText,
+        sender: "ILoveSalads",
+        country_code: cleanCountryCode,
+      }),
+    });
 
-  const data = await response.json();
-  // console.log(data);
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw errorData;
+    }
+
+    return true;
+  } catch (err) {
+    console.error("Error crítico enviando SMS:", err);
+    throw err;
+  }
 };
